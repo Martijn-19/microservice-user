70a15ed585a189bd356fed9563218a07aeeceefc
Martijn-19 <495061@student.fonty.nl>
2023-10-25 11:07:59 +0200

    Added user entity + dto

---
+++ src/User/dto/user.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+import { PrimaryGeneratedColumnIdentityOptions } from 'typeorm/decorator/options/PrimaryGeneratedColumnIdentityOptions';
+
+export class UserDTO {
+  @IsNotEmpty()
+  userID: number;
+  name: string;
+  email: string;
+  password: string;
+  role: Enumerator;
+}
---
+++ src/User/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    userID: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    Role: Enumerator;
+}
